e7702b3bd2269e1bfb3611c0ebbf7891416e4075
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-11-06 21:26:49 +0600

    solved 3rd problem

---
+++ problem-3.ts
@@ -0,0 +1,15 @@
+{
+    const countWordOccurrences = (sentence: string, search: string): number => {
+        let wordCount: number = 0;
+        const spitedSentence = sentence.toLowerCase().replace(/[.,!?]/g, "").split(" ");
+        const searchWord = search.toLowerCase();
+
+        for (let i = 0; i < spitedSentence.length; i++) {
+            if (spitedSentence[i] === searchWord) {
+                wordCount++;
+            }
+        }
+
+        return wordCount;
+    }
+}
